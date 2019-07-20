cf79b19f3a104398721add331c851a0b208fa47a
Justin Galicic <jgalicic@gmail.com>
2019-07-19 22:49:55 -0700

    fixed some mobile responsiveness bugs 3

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
-$( "button" ).click(function() {
- 	$(this).parent().next().slideToggle( "slow" );
-   $(this).children().toggleClass("down");
+$("button").click(function() {
+  $(this)
+    .parent()
+    .next()
+    .slideToggle("slow");
+  $(this)
+    .children()
+    .toggleClass("down");
+});
+
+$(".ferry-title-container").click(function() {
+  $(this)
+    .parent()
+    .next()
+    .slideToggle("slow");
+  $(this)
+    .children()
+    .toggleClass("down");
 });
